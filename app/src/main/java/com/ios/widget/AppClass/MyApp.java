8b51d04a330b64779270a1892a9954095b0d4068
LENOVO <dhrutakasodariya217@gmail.com>
2023-08-11 23:44:55 +0530

    app completed

--- app/src/main/java/com/ios/widget/AppClass/MyApp.java
+++ app/src/main/java/com/ios/widget/AppClass/MyApp.java
@@ -3,6 +3,8 @@ package com.ios.widget.AppClass;
 import android.app.Application;
 import android.os.StrictMode;
 
+import androidx.appcompat.app.AppCompatDelegate;
+
 public class MyApp extends Application{
 
     public static MyApp MyApp = null;
@@ -14,6 +16,7 @@ public class MyApp extends Application{
     @Override
     public void onCreate() {
         super.onCreate();
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
         StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
         StrictMode.setVmPolicy(builder.build());
